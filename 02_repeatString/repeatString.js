0fc9f67305e5f0cc85d33d8aa0bf628efa504401
graymigii <abdullahkassa@gmail.com>
2022-12-15 20:14:36 +0100

    done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, amount) {
 
+    repeatedString = '';
+    for(let i =0; i < amount; i++){
+        repeatedString += word;
+    }
+
+    if(amount >= 0){
+        return repeatedString;
+    }else{
+        return 'ERROR'
+    }
+    
 };
 
 // Do not edit below this line
