f3cc2ca7d3727f27051d3b2694086eab17b72c4e
graymigii <abdullahkassa@gmail.com>
2022-12-15 21:14:19 +0100

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let wordParts = [];
 
+    for (let i = 0; i < word.length; i++){
+        let slicedLetter = word.slice((word.length - i) - 1, (word.length - i));
+        wordParts.push(slicedLetter)
+    }
+
+    let reversedString = '';
+    for (let i = 0; i < wordParts.length; i++){
+        reversedString += wordParts[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
