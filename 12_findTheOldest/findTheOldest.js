c492866d29881e8acfde20ebcf060f796fd93074
graymigii <abdullahkassa@gmail.com>
2022-12-29 12:53:08 +0100

    DONE!!!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const sorted = array.sort((obj1, obj2) => {
+        if (obj1.hasOwnProperty('yearOfDeath') && obj2.hasOwnProperty('yearOfDeath')){
+            return (obj1.yearOfDeath - obj1.yearOfBirth) > (obj2.yearOfDeath - obj2.yearOfBirth)? -1 : 1;
+        }else{
+            const currentYear = new Date().getFullYear();
+            return (currentYear - obj1.yearOfBirth) > (currentYear - obj2.yearOfBirth)? -1 : 1;
+        }     
+        
+    });
 
+    return sorted[0];
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
