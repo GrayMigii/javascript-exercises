74d9ba31c4fc904c32f8b92349db0b0796bdde1d
graymigii <abdullahkassa@gmail.com>
2022-12-29 11:09:33 +0100

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(container) {
+	let sum = 0;
+  container.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(container) {
+  let product = 1;
+  container.forEach(element => {
+    product *= element;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 1 || num == 0){
+    return 1;
+  }else{
+    return num*factorial(num-1)
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const split = word.split('');
 
+    const filtered = split.filter(letter => {
+        if(letter === '!'|| letter === ',' || letter === ' '|| letter === '.' ){
+            return false;
+        }else{
+            return true;
+        }
+    });
+
+    const mapped = filtered.map(letter => {
+        return letter.toLowerCase()
+    });
+
+    let result = true;
+    for(let i = 0; i < mapped.length; i++){
+        if(mapped[i] === mapped.reverse()[i]){
+            result = result && true;
+        }else{
+            result = result && false;
+        } 
+    }
+    return result;
 };
+
+//console.log(palindromes('ZZZZ car, a man, a maracaz.'));
 
 // Do not edit below this line
 module.exports = palindromes;
